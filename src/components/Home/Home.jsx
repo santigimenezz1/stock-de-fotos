99021670f7c708dcf2f54cc02f5c86fe6829f11e
santiago gimenez <santiago.manuel.gimenez97@gmail.com>
2024-01-11 19:06:13 -0300

    valor por defecto peticion

--- src/components/Home/Home.jsx
+++ src/components/Home/Home.jsx
@@ -1,10 +1,16 @@
-import { useState } from "react"
+import { useEffect, useState } from "react"
 import ModalImagen from "../ModalImagen/ModalImagen"
 import NavBar from "../NavBar/NavBar"
 import Main from "./Main/Main"
+import { search } from "../PeticionesFetch/Peticiones"
 
 const Home = () => {
     const[imagenes, setImagenes] = useState([])
+    
+
+    useEffect(()=>{
+        search( "gatos", setImagenes)
+    },[])
 
     return (
         <>
